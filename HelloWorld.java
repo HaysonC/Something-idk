e605c76d167ca049d6dc1123f5f95f086fb937c7
HaysonC <112144740+HaysonC@users.noreply.github.com>
2022-08-28 23:19:59 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
+// Github thingy
 
 public class HelloWorld {
-    public static void main(String[] args) {}
-      System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
-
 }
 
